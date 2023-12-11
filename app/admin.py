d92cf4b6942cb71c68f8bff61ae8c885d58d477b
kavya2854 <gotlurukavya@gmail.com>
2023-12-11 12:23:54 +0530

    first commit

---
+++ app/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from app.models import *
+# Register your models here.
+admin.site.register(Dept)
+
+admin.site.register(Employee)
+
+admin.site.register(Salgrade)
+
+admin.site.register(Bonus)
